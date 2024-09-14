63c256c7fd17720e9b80113e48126da641499c68
bitzhang99 <bitzhang@hotmail.com>
2024-09-14 11:32:52 +0800

    修改了缺陷的版本

--- lily_99.c
+++ lily_99.c
@@ -1,17 +1,18 @@
-<stdio.h>
+#include <stdio.h>
 
 int main() {
     int i,j;
     int n;
-    i=1;j=1;
+    i=1;
+    j=1;
     printf("这是九九乘法表\n");
-    while( i<9 ) {
+    while( j<=9 ) {
         if( i<j ) {
-            printf("%d * %d = %d ", i, j, i*j);
+            printf("%d*%d=%d ", i, j, i*j);
             i++;
         }
         else {
-            printf("%d * %d = %d\n", i, j, i*j);
+            printf("%d*%d=%d\n", i, j, i*j);
             j++;
             i=1;
         }
