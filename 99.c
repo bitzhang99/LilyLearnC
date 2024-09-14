868f25a0c1975d90a80fd8f1de9db60a2df7d29d
bitzhang99 <bitzhang@hotmail.com>
2024-09-14 10:00:29 +0800

    初始化提交九九乘法表

---
+++ 99.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() {
+    int i,j,n;
+    i = 1;
+    j = 1;
+    n = 9;
+    while( i <= n ) {
+		i = 1;
+		while ( i <= j ) {
+        	printf( "%d * %d = %d\t", i, j, i * j);
+			i++;
+		}
+    	j++;
+	printf("\n");
+    }
+}
