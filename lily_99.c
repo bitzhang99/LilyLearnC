78f00084073aa57b2dc74be42afacf10a746e057
bitzhang99 <bitzhang@hotmail.com>
2024-09-14 11:18:03 +0800

    莅莅的初始版本

---
+++ lily_99.c
@@ -0,0 +1,20 @@
+<stdio.h>
+
+int main() {
+    int i,j;
+    int n;
+    i=1;j=1;
+    printf("这是九九乘法表\n");
+    while( i<9 ) {
+        if( i<j ) {
+            printf("%d * %d = %d ", i, j, i*j);
+            i++;
+        }
+        else {
+            printf("%d * %d = %d\n", i, j, i*j);
+            j++;
+            i=1;
+        }
+    }
+    return 0;
+}
