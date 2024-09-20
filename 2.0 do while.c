d2c6793ccc14f00871991383445e94cacb4d5dfc
bitzhang99 <bitzhang@hotmail.com>
2024-09-20 10:15:35 +0800

    增加对程序的评价描述
    对程序格式做了调整

--- 2.0 do while.c
+++ 2.0 do while.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<time.h>
-int main()
-{
+// include 头文件和正文之间有空行，可以看下
+
+int main() {
 	int a = 0;
 	int b = 0;
 	int cnt = 0;
 	srand(time(0));
-	a=rand()%101;
+	a = rand() % 101;
 	printf("我已经在1-100中选择了一个整数，请猜测："); 
 	do{
-		scanf("%d",&b);
+		scanf("%d", &b);
 		cnt++;
-		if(a>b){
+		if(a > b) {
 			printf("猜小了！"); 
 		}
-		if(a<b){
+		/*
+		此处更好的写法是 else if
+		也就是说第一次判断 如果 a>b
+		否则 a 应该是 <= b
+		这个写法下，你有可能第一次就会猜中，你必须考虑第一次猜中的情况，也会走入循环
+		所以，你这地方可以写成 else if(a < b) {
+	         xxxxxx
+		} 
+		接下来的else 可以没有，但良好的程序，为了避免bug，一般最好把else 写全了
+		类似于，你分情况讨论，你的把各个分支都得考虑全，这里面之所以不用考虑 else，
+		是因为else 就是 a = b，这种情况下，在while 那边 就终止循环了，可以到程序
+		外面去处理了
+		*/
+		if(a < b) {
 			printf("猜大了！"); 
 		}
 	}
-	while(a!=b);
+	while(a != b);
+
 	printf("恭喜您猜对了,答案就是%d,您用了%d次猜对。",a,cnt); 
+
 	return 0;
-	} 
+} 
 	
 	
--- detect_chardet.py
+++ detect_chardet.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 # Python 3.5及以上版本的Python解释器默认使用UTF-8编码
 
+# 程序说明
+# 使用chardet进行检测必须使用 b 模式的字符串才行，所以最近解决办法必须从文件中
+# 以二进制读取文件，才能进行检测
+
+
 import sys
 import chardet
 
@@ -16,8 +21,6 @@ def detect_encoding(file_path):
 def read_large_text_file(file_path):
 	encoding = detect_encoding(file_path)
 	print(f"文件{file_path}检测到的编码是：{encoding}")
-
-# text = 'ÎÒÒÑ¾­ÔÚ1-100ÖÐÑ¡ÔñÁËÒ»¸öÕûÊý£¬Çë²Â²â£º'
 
 # print(f"检测到的编码是：{encoding}")
 print("OK")
